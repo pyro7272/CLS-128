c2221f1565c37435ba95d16170fbd506b48884b0
pyro7272 <0PYRO0@gmail.com>
2018-02-20 23:57:40 -0800

    Update animate.js

--- Homework2/JS/animate.js
+++ Homework2/JS/animate.js
@@ -1,8 +1,19 @@
+
 $(document).ready(function() {
     $("#clickMe").click(function() {
         $('#myHeader').animate({ color: 'red' }, 500);
+       
+        
 
-        $('#myHeader').animate({ color: 'white' }, 500);
-        $('#leonS').css('border', "solid 2px red"); 
+        $("#myHeader").css({"font-size": "80px"});
+       
+   
+        $('#leonS').css('border', "solid 2px red");
+
+       
+
+ 
     });
    });
+
+
