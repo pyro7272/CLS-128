32fa9003bbfc175a5b1253b245fd6d4c53c62c54
pyro7272 <0PYRO0@gmail.com>
2018-02-20 21:44:09 -0800

    Update animate.js

--- Homework2/JS/animate.js
+++ Homework2/JS/animate.js
@@ -1,10 +1,12 @@
 
     $("#clickme").click(function() {
-
+console.log
         $("#header").addClass("IncreaseFont", function() {
+           console.log
             $("#header").css("color", "blue").removeClass("IncreaseFont", function() {
+                console.log
                 $(this).css("color", "red");
-            });
+            });console.log
 
         });
 
